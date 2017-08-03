71d6c9dd77ffbbc6f580969152ad7a1cf8bdef58
燕睿涛 <yanruitao@zuoyebang.com>
2017-08-03 23:21:14 +0800

    压缩

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,21 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
 	browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-minify-css');
  
 gulp.task('makeCss', function () {
     gulp.src('less/**/*.less')
         .pipe(less())
+        .pipe(cssmin())
         .pipe(gulp.dest('css'));
 });
 
 gulp.task("browserify", function() {
     return browserify({entries:'./js/index.js'})
         .bundle()
+        .pipe(uglify())
         .pipe(source("bundle.js"))
         .pipe(gulp.dest("build"));
     //定义多个入口文件
@@ -35,7 +39,13 @@ gulp.task("browserify", function() {
     return es.merge.apply(null, tasks);   */     
 });
 
+gulp.task("uglifyJs", function() {
+    gulp.src('build/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+
 gulp.task("watch", function() {
     gulp.watch("less/**/*.less", ["makeCss"]);
-    gulp.watch("js/index.js", ["browserify"]);
+    gulp.watch("js/index.js", ["browserify", "uglifyJs"]);
 });
